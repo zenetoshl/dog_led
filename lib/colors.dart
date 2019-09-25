e2ee01f8e46631d5844229b3135c94e44a8096a4
Jose Neto <zenetoshl@gmail.com>
2019-09-25 11:07:29 -0300

    fixed color screen

--- lib/colors.dart
+++ lib/colors.dart
@@ -13,6 +13,7 @@ class ColorsHome extends StatefulWidget {
 class ColorsHomeState extends State<ColorsHome> {
   LedColors color = LedColors.red; //receber da placa
   bool isOn = false; //tambem receber da placa
+  bool connected = false; //se está conectado com a placa, default será false
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,103 +21,121 @@ class ColorsHomeState extends State<ColorsHome> {
         centerTitle: true,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              '   Ligar Led',
-              style: Theme.of(context).textTheme.title,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ListTile(
-              title: Text(isOn ? 'Ligado!' : 'Desligado!'),
-              leading: new Switch(
-                activeColor: Colors.teal,
-                value: isOn,
-                onChanged: (bool value) {
-                  setState(() {
-                    isOn = value;
-                  });
-                },
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Divider(
-              color: Colors.grey,
-              indent: 30,
-              endIndent: 30,
-              thickness: 1,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              '   Cores',
-              style: Theme.of(context).textTheme.title,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ListTile(
-              title: Text(
-                'Vermelho',
-                style: Theme.of(context).textTheme.subtitle,
-              ),
-              leading: new Radio(
-                activeColor: Colors.red,
-                value: LedColors.red,
-                groupValue: color,
-                onChanged: (LedColors value) {
-                  //mandar para a placa
-                  setState(() {
-                    color = value;
-                  });
-                },
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Verde',
-                style: Theme.of(context).textTheme.subtitle,
-              ),
-              leading: new Radio(
-                activeColor: Colors.green,
-                value: LedColors.green,
-                groupValue: color,
-                onChanged: (LedColors value) {
-                  //mandar para a placa
-                  setState(() {
-                    color = value;
-                  });
-                },
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Azul',
-                style: Theme.of(context).textTheme.subtitle,
-              ),
-              leading: new Radio(
-                activeColor: Colors.blue,
-                value: LedColors.blue,
-                groupValue: color,
-                onChanged: (LedColors value) {
-                  //mandar para a placa
-                  setState(() {
-                    color = value;
-                  });
-                },
-              ),
-            ),
-          ],
+          children: connected
+              ? <Widget>[
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    '   Ligar Led',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ListTile(
+                    title: Text(isOn ? 'Ligado!' : 'Desligado!'),
+                    leading: new Switch(
+                      activeColor: Colors.teal,
+                      value: isOn,
+                      onChanged: (bool value) {
+                        setState(() {
+                          isOn = value;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    indent: 30,
+                    endIndent: 30,
+                    thickness: 1,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    '   Cores',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Vermelho',
+                      style: Theme.of(context).textTheme.subtitle,
+                    ),
+                    leading: new Radio(
+                      activeColor: Colors.red,
+                      value: LedColors.red,
+                      groupValue: color,
+                      onChanged: (LedColors value) {
+                        //mandar para a placa
+                        setState(() {
+                          color = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Verde',
+                      style: Theme.of(context).textTheme.subtitle,
+                    ),
+                    leading: new Radio(
+                      activeColor: Colors.green,
+                      value: LedColors.green,
+                      groupValue: color,
+                      onChanged: (LedColors value) {
+                        //mandar para a placa
+                        setState(() {
+                          color = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Azul',
+                      style: Theme.of(context).textTheme.subtitle,
+                    ),
+                    leading: new Radio(
+                      activeColor: Colors.blue,
+                      value: LedColors.blue,
+                      groupValue: color,
+                      onChanged: (LedColors value) {
+                        //mandar para a placa
+                        setState(() {
+                          color = value;
+                        });
+                      },
+                    ),
+                  ),
+                ]
+              : <Widget>[
+                  SizedBox(
+                    height: 150,
+                  ),
+                  Center(
+                    child: Text(
+                      'Falha de conexão!',
+                      style: Theme.of(context).textTheme.display2,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Placa não conectada.',
+                      style: Theme.of(context).textTheme.display1,
+                    ),
+                  ),
+                ],
         ),
       ),
     );
